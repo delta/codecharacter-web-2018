3697633a88e82ef059e1ab646fbe2e8e61537781
shivashis <shivashispadhi@gmail.com>
2017-10-30 04:22:00 +0530

    Add migrations
    - Add migrations and models

---
+++ api/migrations/20171028223431-create-match.js
@@ -0,0 +1,48 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    queryInterface.createTable('Matches', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      player_id1: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Users',
+          key:'id'
+        }
+      },
+      player_id2: {
+        type: Sequelize.INTEGER,
+        references:{
+          model: 'Users',
+          key:'id'
+        }
+      },
+      ai_id: {
+        type: Sequelize.INTEGER,
+        references:{
+          model:'Ais',
+          key:'id'
+        }
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Matches');
+  }
+};
---
+++ api/models/user.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+	let User = sequelize.define("User", {
+		id: {
+			type:DataTypes.INTEGER, 
+			primaryKey:true
+		},
+		name: {
+			type:DataTypes.STRING
+		},
+		email: {
+			type:DataTypes.STRING
+		},
+		pragyanId: {
+			type:DataTypes.INTEGER
+		},
+		password:{
+			type:DataTypes.STRING
+		},
+		rating:{
+      allowNull: false,
+      type: Sequelize.INTEGER
+    }
+    
+	}, {
+		classMethods: {
+			associate: function(/*models*/) {
+				// associations can be defined here
+			}
+		}
+	});
+	return User;
+};
