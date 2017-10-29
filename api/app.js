69f90c26764434c80c15fc2532f280ba02d1afd3
shivashis <shivashispadhi@gmail.com>
2017-10-30 03:23:32 +0530

    Add /api/configs
    - Rename config files to .js and correct import codes at necessary places
    
    - Add new lines and convert lets to consts when appropriate
    
    - Get environment STAGE from process.env instead of a hard-coded value

--- api/app.js
+++ api/app.js
@@ -1,35 +1,37 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+"use strict";
+const express = require("express");
+const path = require("path");
+const favicon = require("static-favicon");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-//initialize sequelize
-let dbInit = require('./middlewares/dbInit').dbInit;
-dbInit();
+const routes = require("./routes/index");
+const session = require("express-session");
 
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
+const app = express();
+const secretString = require("./config/serverConfig").cookieKey;
+//session setup
+app.use(session({
+	"secret": secretString,
+	"cookie": {
+		"maxAge": 186000000,
+	},
+	"path": "/",
+}));
 app.use(favicon());
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use("/", routes);
+app.use("/users", users);
 
 /// catch 404 and forwarding to error handler
 app.use(function(req, res, next) {
-    var err = new Error('Not Found');
+    var err = new Error("Not Found");
     err.status = 404;
     next(err);
 });
@@ -38,10 +40,10 @@ app.use(function(req, res, next) {
 
 // development error handler
 // will print stacktrace
-if (app.get('env') === 'development') {
+if (app.get("env") === "development") {
     app.use(function(err, req, res, next) {
         res.status(err.status || 500);
-        res.render('error', {
+        res.render("error", {
             message: err.message,
             error: err
         });
@@ -52,7 +54,7 @@ if (app.get('env') === 'development') {
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
     res.status(err.status || 500);
-    res.render('error', {
+    res.render("error", {
         message: err.message,
         error: {}
     });
--- api/middlewares/dbInit.js
+++ api/middlewares/dbInit.js
@@ -1,14 +1,17 @@
 const Sequelize = require('sequelize');
 let sequelize = null;
+const dbCredentials = require("../config/config.js");
+const dbUsername = dbCredentials[process.env.STAGE]["username"];
+const dbPassword = dbCredentials[process.env.STAGE]["password"];
 module.exports.dbInit = ()=>{
-	sequelize = new Sequelize('mysql://root:root@localhost/sample');
-sequelize
-  .authenticate()
-  .then(() => {
-    console.log('Connection has been established successfully.');
-  })
-  .catch(err => {
-    console.error('Unable to connect to the database:', err);
-  });
-}
+	sequelize = new Sequelize("mysql://"+dbUsername+":"+dbPassword+"@localhost/codecharacter");
+	sequelize
+		.authenticate()
+		.then(() => {
+			console.log("Connection has been established successfully.");
+		})
+		.catch(err => {
+			console.error("Unable to connect to the database:", err);
+		});
+};
 module.exports.sequelize = sequelize ;
