11cae0b0e7b2b65ad91d04c3f9bce09e057f05da
plant99 <shivashispadhi@gmail.com>
2017-12-23 10:29:35 +0530

    - Fix lint errors

--- api/migrations/20171029222122-create-constant.js
+++ api/migrations/20171029222122-create-constant.js
@@ -1,29 +1,29 @@
-'use strict';
+"use strict";
 module.exports = {
-  up: (queryInterface, Sequelize) => {
-    return queryInterface.createTable('Constants', {
-      id:{
-        primaryKey: true,
-        autoIncrement: true,
-        type: Sequelize.INTEGER
-      },
-      key: {
-        type: Sequelize.STRING
-      },
-      value: {
-        type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: (queryInterface, Sequelize) => {
-    return queryInterface.dropTable('Constants');
-  }
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable("Constants", {
+			id:{
+				primaryKey: true,
+				autoIncrement: true,
+				type: Sequelize.INTEGER
+			},
+			key: {
+				type: Sequelize.STRING
+			},
+			value: {
+				type: Sequelize.STRING
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE
+			}
+		});
+	},
+	down: (queryInterface) => {
+		return queryInterface.dropTable("Constants");
+	}
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const webpack = require('webpack');
-const path = require('path');
-const OpenBrowserPlugin = require('open-browser-webpack-plugin');
-
+const webpack = require("webpack");
+const path = require("path");
+const OpenBrowserPlugin = require("open-browser-webpack-plugin");
+let __dirname;
 module.exports = {
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-    inline: true,
-    progress: true,
-    contentBase: './app',
-    port: 8080
-  },
-  entry: [
-    'webpack/hot/dev-server',
-    'webpack-dev-server/client?http://localhost:8080',
-    path.resolve(__dirname, 'app/main.jsx')
-  ],
-  output: {
-    path: path.resolve(__dirname, 'build'),
-    publicPath: '/',
-    filename: './bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.css$/, include: path.resolve(__dirname, 'app'), loader: 'style-loader!css-loader' },
-      { test: /\.js[x]?$/, include: path.resolve(__dirname, 'app'), exclude: /node_modules/, loader: 'babel-loader' }
-    ]
-  },
-  resolve: {
-    extensions: ['', '.js', '.jsx']
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new OpenBrowserPlugin({ url: 'http://localhost:8080' })
-  ]
+	devServer: {
+		historyApiFallback: true,
+		hot: true,
+		inline: true,
+		progress: true,
+		contentBase: "./app",
+		port: 8080
+	},
+	entry: [
+		"webpack/hot/dev-server",
+		"webpack-dev-server/client?http://localhost:8080",
+		path.resolve(__dirname, "app/main.jsx")
+	],
+	output: {
+		path: path.resolve(__dirname, "build"),
+		publicPath: "/",
+		filename: "./bundle.js"
+	},
+	module: {
+		loaders: [
+			{ test: /\.css$/, include: path.resolve(__dirname, "app"), loader: "style-loader!css-loader" },
+			{ test: /\.js[x]?$/, include: path.resolve(__dirname, "app"), exclude: /node_modules/, loader: "babel-loader" }
+		]
+	},
+	resolve: {
+		extensions: ["", ".js", ".jsx"]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new OpenBrowserPlugin({ url: "http://localhost:8080" })
+	]
 };
