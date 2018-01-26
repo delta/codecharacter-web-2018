a7d4e61b0c04605543d894f873f610a56960769c
venkat24 <venkatramansrikanth@outlook.com>
2018-01-27 01:31:27 +0530

    Add text content to splash screen

--- src/components/WelcomeScreenComponent.js
+++ src/components/WelcomeScreenComponent.js
@@ -10,9 +10,9 @@ export default class WelcomeScreenComponent extends React.Component {
           <div className="container" style={{position: 'absolute', zIndex: 10, maxWidth: 1800}}>
             <div className="row d-md-flex brand" style={{width: '100%', paddingTop: 50}}>
               <div className="col-md-12 col-sm-12 text-white" style={{position: 'absolute', width: '100%'}}>
-                <h2 className="pt-4" style={{fontSize: 45, zIndex: 100, textShadow: '6px 6px 20px #000000', textAlign: 'center'}}>CODE CHARACTER!</h2>
+                <h2 className="pt-4" style={{fontSize: 45, zIndex: 100, textShadow: '6px 6px 20px #000000', textAlign: 'center'}}>CODE CHARACTER</h2>
                 <p className="mt-5" style={{zIndex: 100, textShadow: '2px 2px 20px #000000', textAlign: 'center'}}>
-                  The Coding Game to challenge yourself and everyone.
+                  The Online AI Challenge
                 </p>
                 <p className="mt-5" style={{textAlign: 'center'}}>
                   <Link to={"/login"} className="btn btn-white mr-2 mb-2 page-scroll">Log In</Link>
@@ -32,15 +32,14 @@ export default class WelcomeScreenComponent extends React.Component {
               </div>
               <div className="col-sm-6 pl-5 pr-5 pt-5 pb-4 wow fadeInRight">
                 <h1>ABOUT</h1>
-                <p className="lead pt-4">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+                <p className="lead pt-4">Code Character is an online AI prgramming competition, where you write C++ code for a real time strategy game. </p><p className="lead pt-4"> Test your code against yourself, against the computer, and then against everyone else! Challenge others to improve your rating as you climb up the leaderboard!</p>
                 <ul className="pt-4 pb-3 list-default">
-                  <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                  <li>Aenean commodo ligula eget dolor.</li>
-                  <li>Aenean massa.</li>
-                  <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
-                  <li>Lorem ipsum dolor sit amet, consectetuer adipiscing elit.</li>
-                  <li>Aenean commodo ligula eget dolor.</li>
-                  <li>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</li>
+                  <li>Integrated code editor and development environment</li>
+                  <li>View your AI playing itself!</li>
+                  <li>Live leaderboard, challenge anyone.</li>
+                  <li>Active discussion forum for any game related questions</li>
+                  <li>Extensive tutorials and documentation</li>
+                  <li>Exciting prizes!</li>
                 </ul>
                 <Link to={"/signup"} className="btn btn-primary mr-2 page-scroll">Get Started</Link>
               </div>
@@ -53,11 +52,10 @@ export default class WelcomeScreenComponent extends React.Component {
               <div className="col-sm-12 pl-5 pr-5 pt-5 pb-4">
                 <h1>FAQ</h1>
                 <ul className="pt-4 pb-3 list-default">
-                  <li><h5>What is the game Called?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p></li>
-                  <li><h5>What is the game Called?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p></li>
-                  <li><h5>What is the game Called?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p></li>
-                  <li><h5>What is the game Called?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p></li>
-                  <li><h5>What is the game Called?</h5><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p></li>
+                  <li><h5>Do I need to know C++ to participate?</h5><p>Yes, some basic knowledge of C++ is definitely required. If you have an understanding of structs, arrays, pointers, and perhaps the STL library, you should be good to go.</p></li>
+                  <li><h5>Do I need to set up the environment on my local machine to code?</h5><p>Not at all. You can compile and run your code directly in the integrated editor.</p></li>
+                  <li><h5>Is the event completely online?</h5><p>Yes! Code Character is played completely on the web.</p></li>
+                  <li><h5>How do I participate?</h5><p>Simply follow the signup link and you'll be on your way!</p></li>
                 </ul>
               </div>
             </div>
@@ -69,7 +67,7 @@ export default class WelcomeScreenComponent extends React.Component {
         <div className="row">
           <div className="col-md-6 offset-md-3 col-sm-8 offset-sm-2 col-xs-12 text-center wow fadeIn">
             <h4 className="pt-2 text-muted">
-                Made with &hearts; by Delta
+                Made with <a href="http://delta.nitt.edu>">&hearts;</a> by <a href="http://delta.nitt.edu>">Delta Force</a>
             </h4>
             </div>
           </div>
