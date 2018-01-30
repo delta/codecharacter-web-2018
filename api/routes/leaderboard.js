47a804b97647daf401bc27963423f5897cfe664a
plant99 <shivashispadhi@gmail.com>
2018-01-30 13:35:40 +0530

    Check for existing matches before pushing an user to leaderboard

--- api/routes/leaderboard.js
+++ api/routes/leaderboard.js
@@ -10,41 +10,32 @@ router.get("/", (req, res)=>{
 		attributes:["id", "name","rating"]
 	})
 		.then((users)=>{
-			/*let ratings = [];
-			users.map((user) => {
-				let retObj = Object.assign({}, {
-					name: user.dataValues.name,
-					id: user.dataValues.id,
-					rating: user.dataValues.rating
-				});
-				ratings.push(retObj);
-			});
-			users.sort((user1, user2) => {
-				if(user1.rating > user2.rating){
-					return -1;
-				}else if(user1.rating < user2.rating){
-					return 1;
-				}
-				return 0;
-			});
-			res.json({success:true, ratings});
-			*/
 			let codeFetchPromises = [];
 			let usersWithLockedCode = [];
 			users.map(user => {
-				let x = models.Code.findOne({
+				let x = models.Match.findOne({
 					where: {
-						user_id: user.id
-					},
-					attributes: ['dll1_locked']
+						$or: [
+							{
+								player_id1: user.id,
+								player_id2: {
+									[Op.not] : user.id
+								}
+							},
+							{
+								player_id1: {
+									[Op.not] : user.id
+								},
+								player_id2: user.id
+							}
+						]
+					}
 				})
-					.then(code => {
-						if(!code){
+					.then(match => {
+						if(!match){
 							return;
 						}
-						if(code.dll1_locked){
-							usersWithLockedCode.push(user);
-						}
+						usersWithLockedCode.push(user);
 					})
 					.catch(err => {
 						//console.log(err);
@@ -92,7 +83,7 @@ router.get('/chunk/:from/:strength', (req, res) => {
 		attributes:['id', 'name', 'rating']
 	})
 		.then( users => {
-			let users = users.reverse();
+			users = users.reverse();
 			let usersSelected = users.slice(req.params.from, req.params.strength);
 			res.json({usersSelected});
 		})
